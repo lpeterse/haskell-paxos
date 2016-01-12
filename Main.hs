fd5cd64473e2f80995747c0a7683fdd38cd03df9
Lars Petersen <info@lars-petersen.net>
2016-01-12 06:25:37 +0100

    Simplified Paxos implementation.

--- Main.hs
+++ Main.hs
@@ -7,39 +7,36 @@ class (Applicative m) => Transmitter m where
   yell :: IndexedMessage -> m ()
   tell :: IndexedMessage -> Member -> m ()
 
-newtype Member = Member Int
-newtype Proposal = Proposal Int
+type    Quorum        = Int
+type    Proposal      = Int
+type    ProposalMap a = IntMap a
 
-data Paxos value
-   = Paxos
-     { memberCount :: Int
-     , log         :: Log
+data State value peer
+   = State
+     { stPeerCount :: Int
+     , stLog       :: IntMap (PaxosState value)
      }
   deriving (Show)
 
-data Log
-   = Log
-     { entries :: IntMap (LogEntry value)
+data PaxosState
+   = Consensus
+     { value         :: value
      }
-  deriving (Show)
-
-data LogEntry
-   = Concensus value
-   | Discussion
-     { promised   :: Proposal
-     , accepted   :: Maybe (Proposal, value)
-     , acceptance :: [Member]
-     }
-  deriving (Show)
-
-data IndexedMessage value
-   = IndexedMessage
-     { index   :: Int
-     , message :: (Message value)
+   | Debate
+     { promised      :: Proposal
+     , accepted      :: Maybe (Proposal, value)
+     , learned       :: S.Set peer
      }
   deriving (Show)
 
 data Message value
+   = Message
+     { msgIndex        :: Int
+     , msgPaxosMessage :: (PaxosMessage value)
+     }
+  deriving (Show)
+
+data PaxosMessage value
    = Announce
      { value    :: value
      }
@@ -48,7 +45,7 @@ data Message value
      }
    | Promise
      { proposal :: Proposal
-     , accepted :: Maybe (Proposal, value)
+     , accepted :: Maybe (Proposal, value, S.Set peer)
      }
    | Accept -- either by proposer or acceptor (as proxy and confirmation)
      { proposal :: Proposal
@@ -56,114 +53,90 @@ data Message value
      }
   deriving (Show)
 
-processMessage :: Transmitter m => Paxos -> Member -> IndexedMessage -> m Paxos
-processMessage st member imsg = do
-  case imsg of
-    IndexedMessage i msg -> do
-      let reply = tell member . IndexedMessage i
-      me <- case msg of
-        -- Announce means immediate end of the debate.
-        -- If we already know about the concensus we do nothing and return the
-        -- previous state. In the other case we store the value in the log.
-        Announce announcedValue -> do
-          case IM.lookup i (log st) of
-            Just (Concensus _) -> pure $ Nothing
-            _                  -> pure $ Just $ Concensus announcedValue
-        Propose currentProposal -> do
-          case IM.lookup i (log st) of
-            -- We tell the sender that concensus has been found.
-            -- This is a non-strictly necessary optimisation.
-            Just (Concensus v) -> do
-              reply $ Announce v
-              pure  $ Nothing
-            Just n@(Discussion _ _ _) -> do
-              case accepted n of
-                -- If we have nothing accepted yet, we can promise.
-                -- We promise the current proposal or the memorized one
-                -- depending on which is higher.
-                Nothing -> do
-                  if currentProposal > promisedProposal
-                    then do
-                      reply $ Promise currentProposal (accepted n)
-                      pure  $ Just  $ n { promised = currentProposal }
-                    else do
-                      reply $ Promise promisedProposal (accepted n)
-                      pure  $ Nothing
-                -- If we have accepted something we need to check whether
-                -- the proposal is eventually obsolete.
-                Just a@(acceptedProposal, acceptedValue) -> do
-                  if currentProposal > acceptedProposal
-                    -- If the proposal is greater than what we have accepted
-                    -- we promise to accept nothing lower than the sender's
-                    -- proposal or the highest proposal we have promised yet.
-                    then if currentProposal > promisedProposal
-                      then do
-                        reply $ Promise currentProposal (accepted n)
-                        pure  $ Just  $ n { promised = currentProposal }
-                      else do
-                        reply $ Promise promisedProposal (accepted n)
-                        pure  $ Nothing
-                    -- It the proposal is lower or equal than what we have
-                    -- already accepted, we just use the `Accept` message to
-                    -- tell the sender although we are not the original proposer.
-                    else do
-                      reply $ Accept acceptedProposal acceptedValue
-                      pure $ Nothing
-        Promise propsalPromisedByPeer acceptedByPeer -> do
-          case IM.lookup i (log st) of
-            Nothing -> case acceptedByPeer of
-              Nothing -> do
-                pure ()
-              Just (proposalAcceptByPeer, valueAcceptedByPeer) -> do
-                
-            Just (Concensus value) -> do
-              reply $ Announce value
-            Just (Discussion proposalPromisedByUs acceptedByUs accepting) -> do
-        Accept proposalAcceptedByPeer valueAcceptedByPeer -> do
-          case IM.lookup i (log st) of
-            Nothing -> do
-              reply $ msg
-              save  $ Discussion
-                      { promised  = proposalAcceptedByPeer
-                      , accepted  = Just (proposalAcceptedByPeer, valueAcceptedByPeer)
-                      , accepting = S.singleton peer
-                      }
-            Just (Concensus value) -> do
-              reply $ Announce value
-            Just discussion@(Discussion proposalPromisedByUs acceptedByUs accepting) -> do
-              case acceptedByUs of
-                Just (proposalAcceptedByUs, valueAcceptedByUs) -> do
-                  case proposalAcceptedByPeer `compare` proposalAcceptedByUs of
-                    -- Tell everyone that we have accepted the new proposal.
-                    GT -> do
-                      yell  $ msg
-                      save  $ Discussion
-                              { promised  = proposalAcceptedByPeer
-                              , accepted  = Just (proposalAcceptedByPeer, valueAcceptedByPeer)
-                              , accepting = S.singleton peer
-                              }
-                    -- We already told everyone that we accepted the value.
-                    -- Telling others again would lead to PING-PONG, so we keep quiet.
-                    -- We just memorize the the peer's acceptance and eventually
-                    -- detect concensus.
-                    EQ -> do
-                      let accepting' = peer `S.insert` accepting
-                      if isMajority accepting'
-                        then do
-                          yell  $ Announce valueAcceptedByUs
-                          save  $ Concensus valueAcceptedByUs
-                        else do
-                          save  $ discussion { accepting = accepting' }
-                    -- Tell peer which higher value we accepted and thereby ask him to
-                    -- accept it too.
-                    LT -> do
-                      reply $ Accept proposalAcceptedByUs valueAcceptedByUs
-                Nothing -> do
-                  reply $ msg
-                  pure  $ Just
-                        $ Discussion
-                          { accepted  = Just (proposalAcceptedByPeer, valueAcceptedByPeer)
-                          , accepting = self `S.insert` remote `S.insert` S.empty
-                          }
-  where
-    isMajority acceptingPeers = (S.size acceptingPeers + 1) * 2 > memberCount st
+processMessage :: Transmitter m => State -> Peer -> Message -> m State
+processMessage state peer (Message i message) = do
+  paxosState   <- pure $ fromMaybe initialState $ IM.lookup i $ stLog state
+  mPaxosState' <- alterPaxosState paxosState peer message
+  pure $ fromMaybe state $ do
+    paxosState <- mPaxosState
+    pure $ state { stLog = IM.insert i paxosState $ stLog state }
+
+alterPaxosState :: Transmitter m => PaxosState -> Quorum -> Peer -> PaxosMessage -> m (Maybe PaxosState)
+alterPaxosState state peer message = case message of
+  -- Announce means immediate end of the discussion.
+  -- If we already know about the concensus we do nothing and return the
+  -- previous state. In the other case we store the value in the log.
+  Announce value ->
+    pure $ case state of
+      Consensus _ -> Nothing
+      Debate _ _  -> Just $ Concensus value
+  -- Propose means the peer wants us to prepare and promise not to accept
+  -- any lower proposals.
+  -- We always send a reply to the peer, but we do not necessarily promise.
+  Propose peerProposal ->
+    case state of
+      -- We tell the sender that concensus has been found.
+      -- This is a non-strictly necessary optimisation.
+      Consensus value ->
+        send peer $ Announce value
+        pure Nothing
+      -- If the proposal is greater than what we have accepted
+      -- we promise to accept nothing lower than the sender's
+      -- proposal or the highest proposal we have promised yet.
+      Debate promisedProposal acceptedProposal _ -> do
+        if peerProposal > promisedProposal
+          then do
+            send peer $ Promise peerProposal acceptedProposal
+            pure  $ Just  $ state { promised = peerProposal, learned = S.empty }
+          else do
+            send peer $ Promise promisedProposal acceptedProposal
+            pure Nothing
+  -- We ignore Promise messages.
+  Promise _ _ -> pure Nothing
+  -- The accept message has a double meaning (Accept and Accepting):
+  -- Its either the peer that asks us to accept or the peer was asked to
+  -- accept and tells all others including us that he accepts.
+  -- It's not necessary to distinguish both cases.
+  Accept peerProposal peerValue ->
+    case state of
+      -- Again: If we know about concensus we tell the peer.
+      Consensus value -> do
+        send peer $ Announce value
+        pure Nothing
+      Debate promisedProposal acceptedProposal learnedPeers ->
+        case peerProposal `compare` promisedProposal of
+          -- An accept request implies the support by a quorum of peers.
+          -- Us not having it promised before is unusual, but valid.
+          -- We broadcast this accept request to signal that we accepted.
+          -- To avoid amplification we broadcast only once per accepted
+          -- proposal. Subsequent accept requests for this proposal will only
+          -- be replied to the requesting peer.
+          GT -> do
+            broadcast message
+            pure  $ Just $ state { promised = peerProposal
+                                 , accepted = Just (peerProposal, peerValue)
+                                 , learned  = S.singleton peer
+                                 }
+          -- An accept request for exactly the proposal that we promised.
+          -- We only respond if have not seen a previous accept from this peer
+          -- to avoid Ping-Pong of Accept messages.
+          LT -> do
+            send peer $ Promise promisedProposal acceptedProposal
+            pure Nothing
+          EQ ->
+            if S.null learnedPeers
+              then do
+                broadcast message
+                pure $ Just $ state { accepted = Just (peerProposal, peerValue)
+                                    , learned  = S.singleton peer
+                                    }
+              else if S.member peer learnedPeers
+                -- Must be a duplicate - just ignore.
+                then pure Nothing
+                then if S.size learned + 2 >= quorum
+                  then do
+                    broadcast $ Announce value
+                    pure $ Just $ Consensus value
+                  else do
+                    send peer message
+                    pure $ Just $ state { learned  = S.insert peer learnedPeers }
